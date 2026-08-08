cf40ece89f366588a43cbcb2711d5cd7bef2b056
Vikash kumar <111779876+vks75202@users.noreply.github.com>
2026-08-09 00:53:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (35.32%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+
+        vector<bool> ans(26,0);
+        for(int i=0;i<sentence.size();i++)
+        {
+            ans[sentence[i]-'a']=1;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(ans[i]==0)
+            return false;
+        }
+        return true;
+    }
+};
