50d74fa8ea3edc8c24e34202ae4daa3f62cbfb3f
Vikash kumar <111779876+vks75202@users.noreply.github.com>
2026-08-25 11:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 24.1 MB (82.33%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        //sort(nums.begin(),nums.end());
+        if(nums.size()==1 && k==nums[0])
+        return k*2;
+        int j;
+        for( j=1;j<=nums.size();j++)
+        {
+            bool flag=false;
+            for(int i=0;i<nums.size();i++){
+            if(nums[i]==k*j)
+            {
+                flag=true;
+                break;
+            }
+            }
+            if(flag==false)
+            return k*j;
+
+        }
+        if(j==nums.size())
+        j++;
+        return k*j;
+    }
+};
