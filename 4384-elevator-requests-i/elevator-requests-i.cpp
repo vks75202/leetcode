287e682e256f03b899645980e497644f85df02ca
Vikash kumar <111779876+vks75202@users.noreply.github.com>
2026-08-25 16:08:33 +0530

    Time: 0 ms (100.00%) | Memory: 30.9 MB (55.87%) - LeetSync

---
+++ 4384-elevator-requests-i/elevator-requests-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int elevatorRequests(int n, vector<int>& requests) {
+        int sum=requests[0];
+        for(int i=1;i<requests.size();i++)
+        {
+            sum+=abs(requests[i-1]-requests[i]);
+        }
+        return sum;
+    }
+};
